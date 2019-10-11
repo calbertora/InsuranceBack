51dda80af85406b16c6bddb1234ff80a8a3c1ff4
Carlos Ramírez <calbertora@gmail.com>
2019-10-11 14:27:45 -0500

    Add the Insurance Controller and the Repository Interface

---
+++ InsuranceApplication/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace InsuranceApplication.Repositories
+{
+    interface IRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity Get(int id);
+
+        void Add(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        void Update(TEntity entity);
+    }
+}

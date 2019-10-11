e0215f0373a23ee6fb9fb5d7b95f0c1ecb097a78
Carlos Ramírez <calbertora@gmail.com>
2019-10-11 14:24:03 -0500

    Add the Insurance Model Class

---
+++ InsuranceApplication/Models/Insurance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InsuranceApplication.Models
+{
+    public class Insurance
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<int> PolicyType { get; set; }
+        public DateTime InitialDate { get; set; }
+        public int Coverage { get; set; }
+        public double Price { get; set; }
+        public int TypeOfRisk { get; set; }
+    }
+}

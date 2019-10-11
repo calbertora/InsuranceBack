a7abd8fd844fc308ddbbeae15eda11f386336f4c
Carlos Ramírez <calbertora@gmail.com>
2019-10-11 14:38:17 -0500

    Add the Context and the EntityFramework package

--- InsuranceApplication/Controllers/InsuranceController.cs
+++ InsuranceApplication/Controllers/InsuranceController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InsuranceApplication.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +10,19 @@ namespace InsuranceApplication.Controllers
 {
     public class InsuranceController : ApiController
     {
+        List<int> policyType = new List<int>();
+
+        [HttpGet]
+        [Route("api/insurance")]
+        public IEnumerable<Insurance> GetAllProducts()
+        {
+            policyType.Add(1);
+            Insurance[] insurances = new Insurance[]
+            {
+                new Insurance { Id = 1, Name = "Insurance 1", Coverage = 4, Description = "Insurance 1 Test", InitialDate = DateTime.Now, PolicyType = policyType, Price = 50, TypeOfRisk = 1},
+                new Insurance { Id = 1, Name = "Insurance 2", Coverage = 4, Description = "Insurance 2 Test", InitialDate = DateTime.Now, PolicyType = policyType, Price = 50, TypeOfRisk = 1}
+            };
+            return insurances;
+        }
     }
 }
---
+++ InsuranceApplication/DAL/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using InsuranceApplication.Models;
+using System.Data.Entity;
+
+namespace InsuranceApplication.DAL
+{
+    public class AppContext : DbContext
+    {
+        public AppContext() : base("AppContext")
+        {
+        }
+        public DbSet<Insurance> Insurances { get; set; }
+    }
+}
